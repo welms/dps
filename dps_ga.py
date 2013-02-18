3519bb7c8f34b06bb837468800109c24fd514609
Welms <nobody@example.com>
2013-02-18 10:25:59 -0800

    complete initial implementation by revamping breeding, mutation and core algorithms
    This is the completed implementation of the single-threaded version that
    can serially run any specified number of populations. Its output has
    been verified against the branch-and-bound algorithms for single and
    multiple target encounters up to 14 seconds (after which
    branch-and-bound starts to take too long to execute).

--- dps_ga.py
+++ dps_ga.py
@@ -1,11 +1,81 @@
+from collections import defaultdict
+from itertools import chain, izip_longest
+from operator import itemgetter
 import random
 import time
+
 from dpslib import *
-from operator import itemgetter
+
+standard_spells = [
+    InitialDamageSpell('Animated Dagger', 0.29, 15, 51178),
+    InitialDamageSpell('Arcane Bewilderment', 0.29, 30, 44607),
+    InitialDamageSpell('Crystal Blast', 1.0, 1.5, 76498),
+    InitialDamageSpell('Earthquake', 1, 10, 77448, 8),
+    InitialDamageSpell('Ice Storm', 1.5, 4.5, 47762, 12),
+    InitialDamageSpell('Petrify', 1.5, 22.5, 75916),
+    InitialDamageSpell("Theurgist's Detonation", 0.29, 30, 106423, 8),
+    DotSpell('Fiery Annihilation', 0.58, 4, 105713, 2, 7368, 12),
+    DotSpell('Shattered Earth', 1.5, 20, 29348, 1.0, 18179, 5.0, 12),
+    DotSpell('Vampire Bats', 1.16, 6, 76292, 4.0, 14522, 24.0),
+    WindsOfVeliousSpell(0.58, 6, 82084, 2.7, 17332, 33.0),
+    ]
+
+def interleave(*iterables):
+    sentinel = object()
+    z = izip_longest(*iterables, fillvalue = sentinel)
+    c = chain.from_iterable(z)
+    f = filter(lambda x: x is not sentinel, c)
+    return list(f)
+
 
 class GaCastHistory(CastHistory):
     """ Adds methods required by a genetic algorithm to the CastHistory
     class."""
+
+    def _best_candidate(self, candidate_spell_lists):
+        return max([
+            GaCastHistory(
+                self._spells,
+                self.num_targets,
+                self.time_limit)._fill_from_spell_list(csl) for csl in
+                    candidate_spell_lists],
+                   key=lambda ch:ch.total_damage)
+
+    def _breed_crossover(self, other):
+        # Self and other are combined by crossover breeding. Since they are
+        # variable length, pick a length along the shortest sequence.
+        min_len = min(len(self), len(other))
+        if min_len < 2:
+            return self
+        # extract a list of the spells cast by each parent
+        parent_spells = []
+        parent_spells.append([sc.spell for sc in self._casts])
+        parent_spells.append([sc.spell for sc in other._casts])
+        # Choose a point between casts where the crossover occurs.
+        xo_point = random.randint(0, min_len - 1)
+        # Combine parent spell lists, producing two children
+        child_spells = [
+            parent_spells[0][:xo_point + 1] + parent_spells[1][xo_point + 1:],
+            parent_spells[1][:xo_point + 1] + parent_spells[0][xo_point + 1:]]
+        return [
+            GaCastHistory(
+                self._spells,
+                self.num_targets,
+                self.time_limit)._fill_from_spell_list(csl)
+            for csl in child_spells]
+
+    def _breed_interleave(self, other):
+        my_spells = [sc.spell for sc in self._casts]
+        other_spells = [sc.spell for sc in other._casts]
+        child_spells = [interleave(my_spells, other_spells),
+                        interleave(other_spells, my_spells)]
+        return [
+            GaCastHistory(
+                self._spells,
+                self.num_targets,
+                self.time_limit)._fill_from_spell_list(csl)
+            for csl in child_spells]
+
 
     def _cast_from_spell_list(self, sl):
         """ Cast as many spells in `sl` as can be cast, in the order they are
@@ -20,7 +90,7 @@ class GaCastHistory(CastHistory):
         presented, then fill in any remaining time optimally, using an
         exhaustive search."""
         self._cast_from_spell_list(sl)
-        return self._optimal_fill()
+        return self.random_fill()
 
     def _optimal_fill(self):
         """Fill whatever time remains in this cast history with the highest
@@ -33,37 +103,46 @@ class GaCastHistory(CastHistory):
             choices.append(copy)
         return max(choices, key=lambda ch:ch.total_damage)
 
+    def _mutate_delete(self):
+        """Delete a spell at random, then optimally fill in the end"""
+        my_spells = [sc.spell for sc in self._casts]
+        delete_idx = random.randint(0, len(my_spells)-1)
+        my_spells.pop(delete_idx)
+        self._casts = []
+        self._cast_from_spell_list(my_spells)
+        self.random_fill()
+        return self
+
+    def _mutate_reverse(self):
+        my_spells = [sc.spell for sc in self._casts]
+        self._casts = []
+        self._fill_from_spell_list(reversed(my_spells))
+        return self
+
+    def _mutate_shuffle(self):
+        my_spells = [sc.spell for sc in self._casts]
+
+        if len(my_spells) > 2:
+            # choose a section to scramble
+            slice_start = random.randint(0, len(my_spells)-2)
+            slice_end = random.randint(slice_start+1, len(my_spells)-1)
+            shuffled_slice = my_spells[slice_start:slice_end]
+            random.shuffle(shuffled_slice)
+            my_spells = (my_spells[:slice_start] +
+                         shuffled_slice +
+                         my_spells[slice_end:])
+        else:
+            random.shuffle(my_spells)
+        self._casts = []
+        self._fill_from_spell_list(my_spells)
+        return self
+
     def breed(self, other):
-        """ Return a (possibly empty) list of new cast histories that are
-        created by combining self and other in some way."""
-        # Self and other are combined by crossover breeding. Since they are
-        # variable length, pick a length along the shortest sequence.
-        min_len = min(len(self), len(other))
-        if min_len < 1:
-            return []
-        # extract a list of the spells cast by each parent
-        parent_spells = []
-        parent_spells.append([sc.spell for sc in self._casts])
-        parent_spells.append([sc.spell for sc in other._casts])
-        # Choose a point between casts where the crossover occurs.
-        xo_point = random.randint(0, min_len - 1)
-        # Combine parent spell lists, producing two children
-        child_spells = [
-            parent_spells[0][:xo_point + 1] + parent_spells[1][xo_point + 1:],
-            parent_spells[1][:xo_point + 1] + parent_spells[0][xo_point + 1:]]
-        # The child spell lists as they stand may not actually be fully
-        # castable, or may not fully utilize the cast history time limits.
-        # Create a cast history that is as similar to the child spell list as
-        # possible but still fully utilizes the encounter time limit. Because
-        # of these restrictions, it is possible that the child will be a clone
-        # of a parent.
-        candidates = [
-            GaCastHistory(self._spells, self.num_targets, self.time_limit),
-            GaCastHistory(self._spells, self.num_targets, self.time_limit)]
-        for i, ch in enumerate(candidates):
-            ch._cast_from_spell_list(child_spells[i])
-        child = max(candidates, key=lambda ch:ch.last_ability_recovery_time)
-        return child._optimal_fill()
+        """ Return a new cast history that is created by combining self and
+        other in some way."""
+        if random.random() < 0.2:
+            return self._breed_crossover(other)
+        return self._breed_interleave(other)
 
     def duplicate(self):
         """ Return a copy of this object which can be modified without affecting
@@ -78,24 +157,9 @@ class GaCastHistory(CastHistory):
         """Mutate this cast history by some method."""
         # There are lots of ways to mutate a sequence: deletion, insertion,
         # scrambling, etc.
-        my_spells = [sc.spell for sc in self._casts]
-
-        if len(my_spells) > 2:
-            original = my_spells[:]
-            while original == my_spells:
-                # choose a section to scramble
-                slice_start = random.randint(0, len(my_spells)-2)
-                slice_end = random.randint(slice_start+1, len(my_spells)-1)
-                shuffled_slice = my_spells[slice_start:slice_end]
-                random.shuffle(shuffled_slice)
-                my_spells = (my_spells[:slice_start] +
-                             shuffled_slice +
-                             my_spells[slice_end:])
-        else:
-            random.shuffle(my_spells)
-        self._casts = []
-        self._fill_from_spell_list(my_spells)
-        return self
+        op = random.choice([self._mutate_delete, self._mutate_shuffle,
+                            self._mutate_reverse])
+        return op()
 
     def random_fill(self):
         """Fill this cast history with randomly selected spells."""
@@ -106,14 +170,23 @@ class GaCastHistory(CastHistory):
         return self
 
 
-def crossover(population_cdf):
+def breed(population_cdf):
     parent1 = None
     parent2 = None
     assert len(population_cdf) > 1
     while parent1 == parent2:
-        parent1 = pick(population_cdf)
-        parent2 = pick(population_cdf)
-    return parent1.breed(parent2)
+        parent1 = pick_proportionate(population_cdf)
+        parent2 = pick_proportionate(population_cdf)
+    return [parent1, parent2] + parent1.breed(parent2)
+
+
+def diversity(population):
+    """Return a number representing the genetic diversity of the given
+    population."""
+    damages = defaultdict(int)
+    for ch in population:
+        damages[ch.total_damage] += 1
+    return len(damages)
 
 
 def make_population_cdf(population):
@@ -123,111 +196,142 @@ def make_population_cdf(population):
         population_cdf.append(
             [population_cdf[i-1][0] + damages[i],
              population[i]])
-    for pair in population_cdf:
-        pair[0] /= population_cdf[-1][0]
     return population_cdf
 
 
-def pick(cdf_list):
-    assert cdf_list[-1][0] == 1.0
-    val = random.random()
+def pick_proportionate(cdf_list):
+    val = random.randint(0, int(cdf_list[-1][0]))
     for partition, choice in cdf_list:
         if partition >= val:
-            assert choice
             return choice
     # shouldn't get here
     assert False, (
         'pick: last partition value {}'
-        ' smaller than random.uniform value {}'.format(
+        ' smaller than random val {}'.format(
             cdf_list[-1][0], val))
     return cdf_list[-1][1]
 
 
-def find_best_cast_history(spells, num_targets, time_limit):
+def pick_tournament(cdf_list):
+    tournament_size = 4
+    best = None
+    for i in range(tournament_size):
+        choice = random.choice(cdf_list)[1]
+        if best is None or choice.total_damage > best.total_damage:
+            best = choice
+    return best
+
+
+def report(gen, population):
+    best = max(population, key=lambda x:x.total_damage)
+    damages = defaultdict(int)
+    for ch in population:
+        damages[ch.total_damage] += 1
+    total_damage = 0.0
+    total_count = 0
+    for dmg, count in damages.iteritems():
+        total_damage += dmg * count
+        total_count += count
+    mean = total_damage / total_count
+    print 'gen:{:< 4}  bins:{:< 4}  mean:{:< 8,.0f}  best: {:,.0f}'.format(
+        gen, len(damages), mean, best.total_damage)
+    return len(damages), best
+
+
+def find_best_cast_history(spells, num_targets, time_limit, elite=None):
     start_time = time.time()
-    MAX_POPULATION = 100
-    MAX_GENERATIONS = 50
-    MUTATION_CHANCE = 0.01
-    CULL_PCT = 0.03
-    ELITE_PCT = 0.03
+    MAX_POPULATION = 1000
+    MAX_GENERATIONS = 100
+    MAX_MUTATION_CHANCE = 0.5
+    MIN_MUTATION_CHANCE = 0.001
+    BREED_PCT = 1.0
+    MIN_BIN_PCT = 0.01
 
     # create an initial population
-    population = [
-        GaCastHistory(spells, num_targets, time_limit).random_fill() for
-        i in xrange(MAX_POPULATION)]
-
-    # iteratively improve the population until no improvement appears in
-    # successive generations or we've reached the maximum number of
-    # generations.
-    best_ever = None
-    for generation in xrange(MAX_GENERATIONS):
-        # Culling: discard the lowest-performing elements of the population
-        population.sort(key=lambda ch:ch.total_damage, reverse=True)
-        population = population[:-int(len(population) * CULL_PCT)]
-        # Elitism: always propagate the best-performing individuals
-        next_gen = population[:int(len(population) * ELITE_PCT)]
+    if elite:
+        population = elite
+    else:
+        population = []
+    while len(population) < MAX_POPULATION:
+        population.append(
+            GaCastHistory(spells, num_targets, time_limit).random_fill())
+    # report the best of the randomly generated population
+    bins, best_ever = report(0, population)
+    min_bins = int(MIN_BIN_PCT * MAX_POPULATION + 0.5)
+    num_to_breed = int(BREED_PCT * MAX_POPULATION)
+    # Iteratively improve the population
+    nudge = (MAX_MUTATION_CHANCE - MIN_MUTATION_CHANCE) / MAX_GENERATIONS
+    cur_mutation_chance = MAX_MUTATION_CHANCE
+    for generation in xrange(1, MAX_GENERATIONS+1):
         # Use the fitness function to make a cumulative distribution function
         # for roulette-wheel selection.
         population_cdf = make_population_cdf(population)
-        # Fill the next generation by breeding and mutating the current
-        # population.
-        while len(next_gen) < MAX_POPULATION:
-            result = crossover(population_cdf)
-            if random.random() < MUTATION_CHANCE:
-                result.mutate()
-            next_gen.append(result)
-        # Report on the results
-        best = max(next_gen, key=lambda x:x.total_damage)
-        if best_ever is None or best.total_damage > best_ever.total_damage:
-            best_ever = best.duplicate()
-        print 'gen {: 3}: pop {} {:,} dmg {}'.format(
-            generation, len(next_gen), best.total_damage, repr(best))
-        population = next_gen
-    print 'run complete in {}'.format(timedelta(seconds=time.time() - start_time))
-    print 'best ever for num_targets={} time_limit={}:'.format(
-        num_targets, time_limit)
-    print '{:,.0f} damage, ({:,.0f} dps)'.format(
-        best_ever.total_damage,
-        best_ever.total_damage / time_limit)
-    best_ever.print_history()
-    return best_ever
+        children = []
+        cur_mutation_chance -= nudge
+        #print 'mutation chance', cur_mutation_chance
+        for i in xrange(num_to_breed):
+            result = breed(population_cdf)
+            if random.random() < cur_mutation_chance:
+                result[-1].mutate()
+            if random.random() < cur_mutation_chance:
+                result[-2].mutate()
+            children += result[2:]
+        # init next generation to current population plus children
+        next_gen = population + children
+        next_gen_cdf = make_population_cdf(next_gen)
+        population = [best_ever]
+        #while len(population) < MAX_POPULATION:
+            #population.append(pick_tournament(next_gen_cdf))
+        # sort next generation by fitness, descending
+        next_gen.sort(key=lambda ch:ch.total_damage, reverse=True)
+        # take the fittest of the previous population plus the children as the
+        # next generation.
+        population = next_gen[:MAX_POPULATION]
+        bins, best = report(generation, population)
+        if best.total_damage > best_ever.total_damage:
+            best_ever = best
+        if bins < min_bins:
+            break
+    return [best_ever]
 
+def td(s):
+    time_str = str(timedelta(seconds=int(s)))
+    if time_str.startswith('0:'):
+        return time_str[2:]
+    return time_str
+
+
+def multirun(num_targets, enc_time_limit, elite=None):
+    MAX_RUNS = 8
+    start_time = time.time()
+    for i in range(MAX_RUNS):
+        elite = find_best_cast_history(standard_spells, num_targets, enc_time_limit, elite)
+        cumulative_time = time.time() - start_time
+        time_per_run = cumulative_time / (i + 1)
+        est_total_time = time_per_run * MAX_RUNS
+        etr = est_total_time - cumulative_time
+        best = elite[0]
+        print 'run {: 3}: {:,.0f} damage, ({:,.0f} dps) {}'.format(
+            i+1,
+            best.total_damage,
+            best.total_damage / enc_time_limit,
+            repr(best))
+        print '  etr:', td(etr)
+    print 'run complete in', td(time.time() - start_time)
+    print 'best ever for num_targets={} time_limit={}:'.format(
+        num_targets, enc_time_limit)
+    print '{:,.0f} damage, ({:,.0f} dps)'.format(
+        best.total_damage,
+        best.total_damage / enc_time_limit)
+    best.print_history()
+    return best
 
 def main():
-    random.seed()
-    spells = [
-        InitialDamageSpell('Animated Dagger', 0.29, 15, 51178),
-        InitialDamageSpell('Arcane Bewilderment', 0.29, 30, 44607),
-        InitialDamageSpell('Crystal Blast', 1.0, 1.5, 76498),
-        InitialDamageSpell('Earthquake', 1, 10, 77448, 8),
-        InitialDamageSpell('Ice Storm', 1.5, 4.5, 47762, 12),
-        InitialDamageSpell('Petrify', 1.5, 22.5, 75916),
-        InitialDamageSpell("Theurgist's Detonation", 0.29, 30, 106423, 8),
-        DotSpell('Fiery Annihilation', 0.58, 4, 105713, 2, 7368, 12),
-        DotSpell('Shattered Earth', 1.5, 20, 29348, 1.0, 18179, 5.0, 12),
-        DotSpell('Vampire Bats', 1.16, 6, 76292, 4.0, 14522, 24.0),
-        WindsOfVeliousSpell(0.58, 6, 82084, 2.7, 17332, 33.0),
-        ]
-    find_best_cast_history(spells, 1, 12)
-
-
-def test_pick():
-    random.seed()
-    cdf = [[1, 'a']]
-    for i in xrange(2, 11):
-        prev = cdf[i-2]
-        cdf.append([i+prev[0], chr(ord(prev[1])+1)])
-    for pair in cdf:
-        pair[0] /= float(cdf[-1][0])
-    pick_count = {}
-    for i, letter in cdf:
-        pick_count[letter] = 0
-    for i in xrange(100):
-        pick_count[pick(cdf)] += 1
-    assert pick_count[cdf[0][1]] < pick_count[cdf[-1][1]]
-    for i, letter in cdf:
-        print letter, '#' * pick_count[letter]
-
+    num_targets = 1
+    time_limit = 9.5
+    ch = GaCastHistory(standard_spells, num_targets, time_limit)
+    #ch.from_repr('<CastHistory [SE,E,IS,WV,FA,CB,VB,IS,FA,P,E,TD,FA,CB,IS,CB,FA,SE,CB,IS,E,FA,AD,AB]>')
+    multirun(num_targets, time_limit)
 
 if __name__ == '__main__':
     main()
